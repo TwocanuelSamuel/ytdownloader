5c199d34bb032ae0d280d7185476139bbdb21411
SamTwocan <samuelwiseman01@gmail.com>
2026-05-22 17:32:59 +1000

    Making her pretty

--- multidl.py
+++ multidl.py
@@ -1,9 +1,10 @@
-#########################################################3
-#
-#   Youtube Video Downloader
-#   This project is a translation of 
-
-#
+##############################################################################################################################
+#                                                                                                                            #
+#   Youtube Video Downloader                                                                                                 #
+#   This project is a translation of https://github.com/Tomshiii/ahk/blob/main/Streamdeck%20AHK/download/mult-dl.ahk         #
+#   to work more broadly across different operating systems (namely Linux as I transitioned to it)                          #
+#                                                                                                                            #
+##############################################################################################################################
 
 import tkinter as tk
 from tkinter import ttk
