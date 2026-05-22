1d4b197c9d60df400d1954982cf029f325d3dd58
SamTwocan <samuelwiseman01@gmail.com>
2026-05-22 17:27:13 +1000

    first commit

---
+++ multidl.py
@@ -0,0 +1,14 @@
+#########################################################3
+#
+#   Youtube Video Downloader
+#   This project is a translation of 
+
+#
+
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("Multi Download")
+
+root.mainloop()
