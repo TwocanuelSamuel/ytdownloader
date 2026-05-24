49ff68720a755ca3886f7ed9994672b9795c8373
SamTwocan <samuelwiseman01@gmail.com>
2026-05-24 18:58:30 +1000

    gui functionality now includes downloading audio, choosing filetype (4 options) and selecting file destination

--- multidl.py
+++ multidl.py
@@ -1,10 +1,10 @@
-##############################################################################################################################
-#                                                                                                                            #
-#   Youtube Video Downloader                                                                                                 #
-#   This project is a translation of https://github.com/Tomshiii/ahk/blob/main/Streamdeck%20AHK/download/mult-dl.ahk         #
-#   to work more broadly across different operating systems (namely Linux as I transitioned to it)                           #
-#                                                                                                                            #
-##############################################################################################################################
+
+                                                                                                                            
+#   Youtube Video Downloader                                                                                                 
+#   This project is a translation of https://github.com/Tomshiii/ahk/blob/main/Streamdeck%20AHK/download/mult-dl.ahk         
+#   to work more broadly across different operating systems (namely Linux as I transitioned to it)                           
+                                                                                                                           
+
 
 import tkinter as tk
 from tkinter import ttk
